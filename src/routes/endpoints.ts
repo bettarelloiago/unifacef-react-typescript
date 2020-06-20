a29b544067bbe25e694572a1d781a585a224a331
Iago Bettarello <bettarello.iago@gmail.com>
2020-06-20 15:48:50 -0300

    Implementado cache

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -4,6 +4,7 @@ import Home from '../containers/home';
 import Combustivel from '../containers/combustivel';
 import StarWars from '../containers/star-wars';
 import StarWarsDetails from '../containers/star-wars-details';
+import Cache from '../containers/cache';
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -15,7 +16,8 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/`, component: Home, exact: true },
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
-  { path: `${publicUrl}/star-wars`, name: 'StarWars', component: StarWars, exact: true },
+  { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
   { path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails, exact: true },
+  { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
 ];
