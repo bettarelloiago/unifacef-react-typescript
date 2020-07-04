ea0453505890ecf063b7929e775856a8b96f31aa
Iago Bettarello <bettarello.iago@gmail.com>
2020-07-04 09:08:47 -0300

    Adicionado OneSignal

--- src/apis/economy.api.ts
+++ src/apis/economy.api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { configs } from '../configs';
 
 export const getPrice = async () => {
-  return axios.request({method: 'GET', url: 'https://economia.awesomeapi.com.br/json/all' })
+  return axios.request({method: 'GET', url: configs.apis.economia })
   // return axios.get('https://economia.awesomeapi.com.br/json/all');
 }
